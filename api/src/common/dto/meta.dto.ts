99d7d892683137f578e90589146aa03c0d9d577c
marcel lukco <lukcomarcel@gmail.com>
2025-10-31 11:41:22 +0100

    feat: edit totalCount

--- api/src/common/dto/meta.dto.ts
+++ api/src/common/dto/meta.dto.ts
@@ -5,13 +5,4 @@ export class MetaDto {
     description: 'Total number of items across all pages',
   })
   totalCount?: number;
-
-  @ApiPropertyOptional({ description: 'Current page index (1-based)' })
-  page?: number;
-
-  @ApiPropertyOptional({ description: 'Number of items per page' })
-  pageSize?: number;
-
-  @ApiPropertyOptional({ description: 'Total number of pages' })
-  totalPages?: number;
 }
